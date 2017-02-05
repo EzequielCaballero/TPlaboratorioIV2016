ac64a39c558b6efad85fee3c64c37bb56ffbee01
EzequielCaballero <ezequiel.caballero33@gmail.com>
2017-02-05 20:59:59 -0300

    Validación ante pérdida de $stateParams

--- scripts/controllers/Local/controladorLocalOpciones.js
+++ scripts/controllers/Local/controladorLocalOpciones.js
@@ -6,38 +6,42 @@ angular.module('ABMangularAPI.controladorLocalOpciones', [])
     console.info("SESION ACTIVA: ", $sesion);
 
     if(!$auth.isAuthenticated())
+      $state.go("inicio");
+
+    if($stateParams.obj == null)
+      $state.go("cliente.inicio");
+    else
     {
-       $state.go("inicio");
-    }
+        $scope.local = $stateParams.obj;
+        console.info("Parametro datos: ", $stateParams);
+        $scope.direccionLocal = $scope.local.direccion;
+        console.info("Local traido: ", $scope.local);
 
-    $scope.local = $stateParams.obj;
-    console.info("Parametro datos: ", $stateParams);
-    $scope.direccionLocal = $scope.local.direccion;
-    console.info("Local traido: ", $scope.local);
+        $scope.disabledProductos = false;
+        $scope.disabledOfertas = false;
 
-    $scope.disabledProductos = false;
-    $scope.disabledOfertas = false;
+        $scope.visualizar = function(accion){
+          switch(accion)
+          {
+            case "Productos":
+              $scope.verProductos = true;
+              $scope.verOfertas = false;
+              $scope.disabledProductos = true;
+              $scope.disabledOfertas = false;
+              break;
+            case "Ofertas":
+              $scope.verProductos = false;
+              $scope.verOfertas = true;
+              $scope.disabledProductos = false;
+              $scope.disabledOfertas = true;
+              break;
+          }
+        }
 
-    $scope.visualizar = function(accion){
-      switch(accion)
-      {
-        case "Productos":
-          $scope.verProductos = true;
-          $scope.verOfertas = false;
-          $scope.disabledProductos = true;
-          $scope.disabledOfertas = false;
-          break;
-        case "Ofertas":
-          $scope.verProductos = false;
-          $scope.verOfertas = true;
-          $scope.disabledProductos = false;
-          $scope.disabledOfertas = true;
-          break;
-      }
-    }
-
-    $scope.cerrarSesion = function(){
-        $auth.logout();
-        $state.go("inicio");
-    }
+        $scope.cerrarSesion = function(){
+            $auth.logout();
+            $state.go("inicio");
+        }
+        
+    }//FIN DE LA CONDICION ELSE
 });
