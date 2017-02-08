66a8b64b750b4309e0aeae34cd0df15c52812e77
EzequielCaballero <ezequiel.caballero33@gmail.com>
2017-02-07 23:15:40 -0300

    Foto productos (todo o por oferta)

--- scripts/controllers/Local/controladorLocalOpciones.js
+++ scripts/controllers/Local/controladorLocalOpciones.js
@@ -37,17 +37,74 @@ angular.module('ABMangularAPI.controladorLocalOpciones', [])
                 console.log("FALLO! ", response);
         });
 
-        //OPCIONES DE GRILLA OFERTAS
+        //TRAER PRODUCTOS (según oferta)
         $scope.verProductosDeOferta = function(oferta){
 
           servicioRetornoProductos.traerCiertosProductos(oferta).then(function(respuesta){
+          $scope.lista_productos_oferta = respuesta.data;
           console.info("Productos asociados a oferta:", respuesta.data);
+
+          $scope.imagenProductos($scope.lista_productos_oferta, 'oferta');
+          $scope.verSoloProductos = false;
+          $scope.verProductosPorOferta = true;
+          // $("#verProductos").on("shown.bs.modal", function(e) {
+          // });
 
           },function errorCallback(response) {
                 console.log("FALLO! ", response);
           });
 
         }
+
+          //FOTOS EN MODAL (PRODUCTOS)
+          $scope.imagenProductos = function(producto, criterio){
+        
+          var slideIndex = 1;
+
+          function mostrarFoto(n) {
+            var i;
+            var x = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("demo");
+            if (n > x.length) {slideIndex = 1}    
+            if (n < 1) {slideIndex = x.length}
+            for (i = 0; i < x.length; i++) {
+               x[i].style.display = "none";  
+            }
+            for (i = 0; i < dots.length; i++) {
+               dots[i].className = dots[i].className.replace(" w3-white", "");
+            }
+            x[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " w3-white";
+          }
+
+          $scope.moverFoto = function(n) {
+            mostrarFoto(slideIndex += n);
+          }
+
+          $scope.elegirFoto = function(n) {
+            mostrarFoto(slideIndex = n);
+          }
+
+          if(criterio=='solo')
+          {
+              $scope.verSoloProductos = true;
+              $scope.verProductosPorOferta = false;
+              $scope.tituloGaleria = producto.nombre;
+              console.info("Titulo", $scope.tituloGaleria);
+              $scope.fotoProducto_1 = "img/Productos/" + producto.foto1;
+              $scope.fotoProducto_2 = "img/Productos/" + producto.foto2;
+              $scope.fotoProducto_3 = "img/Productos/" + producto.foto3;
+              console.info("Producto info: ", producto);
+          }
+          else
+          {
+            $scope.tituloGaleria = "Productos asociados a la oferta";
+            $('#verProductos').modal('show');
+          }
+
+          setTimeout(function(){ mostrarFoto(slideIndex); }, 300);
+
+        }//FIN de función imagenProductos
 
         //OPCIONES DE MENU
         $scope.visualizar = function(accion){
