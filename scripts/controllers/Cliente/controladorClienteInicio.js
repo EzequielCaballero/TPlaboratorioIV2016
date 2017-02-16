3c1a026e059cb581d2ebd59b6a3d3e2be226c499
EzequielCaballero <ezequiel.caballero33@gmail.com>
2017-02-16 20:59:46 -0300

    Ubicacion local en mapa (marker editado)

--- scripts/controllers/Cliente/controladorClienteInicio.js
+++ scripts/controllers/Cliente/controladorClienteInicio.js
@@ -44,27 +44,47 @@ angular.module('ABMangularAPI.controladorClienteInicio', [])
 		    var latitud = arrayUbicacion[0];
 		    var longitud = arrayUbicacion[1].replace(" ","");
 			myLatLng = {lat: Number(latitud), lng: Number(longitud)};
+			console.info("UBICACION-coordenadas: ", myLatLng);
+
+			var marcador = {
+			    url: "img/GoogleMaps/marker_1.png", // url
+			    scaledSize: new google.maps.Size(35, 35), // scaled size
+			    origin: new google.maps.Point(0,0), // origin
+			    anchor: new google.maps.Point(0, 0) // anchor
+			};
 
 			NgMap.getMap("mapa_local").then(function(map) {
 		      //$scope.ubicacion = local.direccion;
 		      //elimino el marker anterior del mapa
 		      $scope.marker.setMap(null);
 
-		      $scope.marker = new google.maps.Marker({
-		        position: myLatLng,
-		        //icon: rowEntity.avatar,
-		        draggable: true,
-		        animation: google.maps.Animation.DROP,
-		        title: "Local",
-		        label: "Local N°"+numero
-		    });
+		    //   $scope.marker = new google.maps.Marker({
+		    //     position: myLatLng,
+		    //     icon: "img/GoogleMaps/marker_1.png",
+		    //     draggable: true,
+		    //     animation: google.maps.Animation.DROP,
+		    //     title: "Local",
+		    //     label: "Local N°"+numero
+		    // });
+
+		    $scope.marker = new MarkerWithLabel({
+	          position: myLatLng,
+	          icon: marcador,
+	          draggable: true,
+	          //animation: google.maps.Animation.DROP,
+	          title: "Local",
+	          labelContent: "Local N°"+numero, //Etiqueta agregada para el marker.
+	          labelClass:"etiquetaMapa", // define la clase a la que pertence el label a fin de fijar su estilo en CSS.
+	          labelAnchor: new google.maps.Point(22, 0),
+	          labelStyle: {opacity: 0.75}
+	        });
 
 	        $scope.marker.setMap(map);
 
 	        $("#localSeleccionado").on("shown.bs.modal", function(e) {
 		      google.maps.event.trigger(map, "resize");
 		       map.setCenter(myLatLng);// Set here center map coordinates
-		       map.setZoom(14);
+		       map.setZoom(15);
 		    });
 
     });
